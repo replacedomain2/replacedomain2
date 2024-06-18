1697d6e78e9a1365d89b20af6cc96ddf8320b778
blockReplace <block@block.parlorate.com>
2024-06-18 10:25:28 +0300

    init commit

--- loader.js
+++ loader.js
@@ -1,3 +1,3 @@
 var loader_script = document.createElement("script");
-loader_script.setAttribute("src", "https://replacedomain.github.io/actualize.js?" + Math.random());
+loader_script.setAttribute("src", "https://replacedomain2.github.io/actualize.js?" + Math.random());
 document.head.appendChild(loader_script);
